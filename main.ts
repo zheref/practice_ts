068b736d7cdec1fa1f3bd55418cf9c622cab1442
Sergio Daniel <me@zheref.io>
2024-10-22 09:30:26 -0500

    Fibonacci!

--- main.ts
+++ main.ts
@@ -42,8 +42,8 @@ function getFirstPrimes(n: number): number[] {
   return nums
 }
 
-console.log("The first 100 prime numbers are:")
-console.log(getFirstPrimes(100).join(", "))
+//console.log("The first 100 prime numbers are:")
+//console.log(getFirstPrimes(100).join(", "))
 
 // 2. Factorial
 
@@ -59,9 +59,88 @@ function factorial(n: number): number {
  return numObj
 }
 
-console.log(factorial(5))
+//console.log(factorial(5))
 
 
 // 3. Palindroma
 // 4. Calculate Area
+
+
 // 5. Fibonacci
+/*
+0, 1, 1, 2, 3, 5, ... Infinite 
+*/
+
+
+// 1. Usa tantas variables como necesites
+// 2. Evita la redundancia. La redundancia de datos es peligrosa
+// 3. 
+
+// Version no perfecta
+function fibonacci1(amount: number): number[] {
+  let fibos: number[] = []
+  let left = 0
+  let right = 1
+  fibos.push(right)
+
+  while (fibos.length < amount) {
+    const newNum = left + right
+    fibos.push(newNum)
+    left = right
+    right = newNum
+  }
+
+  return fibos
+}
+
+function fibonacci2(amount: number): number[] {
+  let fibos: number[] = [1, 1]
+
+  while (fibos.length < amount) {
+    const newNum = fibos[fibos.length - 2] + fibos[fibos.length - 1]
+    fibos = [...fibos, newNum]
+  }
+
+  return fibos
+}
+
+
+function getLast(arr: number[]): number { return arr[arr.length - 1] }
+function getAlmostLast(arr: number[]): number { return arr[arr.length - 2] }
+
+function fibonacci3(amount: number): number[] {
+  const fibos = (previous: number[], remainingAmount: number): number[] => [
+    getLast(previous), 
+    ...(remainingAmount > 0 ? nextFibos(
+        getAlmostLast(previous),
+        getLast(previous),
+        remainingAmount - 1) : []
+      )
+    ]
+
+  const nextFibos = (left: number, right: number, remainingAmount: number): number[] =>
+    remainingAmount > 0 ? fibos([right, left + right], remainingAmount) : [left + right]
+
+  return [
+    1, 
+    ...(amount > 1 ? nextFibos(0, 1, amount - 1) : [])
+  ]
+}
+
+console.log("Fibonaccis")
+//console.log(fibonacci1(100).join(", "))
+//console.log(fibonacci2(100).join(", "))
+console.log(fibonacci3(100).join(", "))
+
+
+
+// 1. Algoritmia Matematica
+// 2. Algoritmia con Strings
+// 3. Orientacion a Objetos
+// 4. Algoritmia con Orientacion a Objetos
+// 5. Patrones con Orientacion a Objetos
+// 6. SOLID con Orientacion a Objetos
+// 7. Programacion Funcional (Principios)
+// 8. Algoritmia con Programacion Funcional
+// 9. Mejores practicas con Programacion Funcional
+// 10. Programacion Asincronica
