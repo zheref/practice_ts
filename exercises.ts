5086ddc8799bb92fc6192eeaeec697f3fb05519d
Maria Alejandra <mariaalejandraqg@gmail.com>
2024-10-22 22:40:23 -0500

    Solucion Ejercicio 1

--- exercises.ts
+++ exercises.ts
@@ -7,22 +7,22 @@ function numbersSequence(step: number, limit: number): number[] {
 }
 
 // Expectativas
-numbersSequence(1, 7) // [1, 2, 3, 4, 5, 6, 7]
-numbersSequence(2, 10) // [2, 4, 6, 8, 10]
-numbersSequence(3, 20) // [3, 6, 9, 12, 15, 18]
+//numbersSequence(1, 7) // [1, 2, 3, 4, 5, 6, 7]
+//numbersSequence(2, 10) // [2, 4, 6, 8, 10]
+//numbersSequence(3, 20) // [3, 6, 9, 12, 15, 18]
 
 // 2. Escribir funcion que calcule raiz cuadrada del valor absoluto de un numero dado
-sqtabs(10) // 3.16227766
-sqtabs(-10) // 3.16227766
-sqtabs(-49) // 7
-sqtabs(64) // 8
+//sqtabs(10) // 3.16227766
+//sqtabs(-10) // 3.16227766
+//sqtabs(-49) // 7
+//sqtabs(64) // 8
 
 // 3. Escribir una function que calcule el factorial de un numero, de forma funcional
 
 // Expectativas
-factorial(6) // 720
-factorial(10) // 3628800
-factorial(15) // 1307674368000
+//factorial(6) // 720
+//factorial(10) // 3628800
+//factorial(15) // 1307674368000
 
 // 4. Escribir funcion que calcule la cantidad de segundos, dada una cantidad de dias, horas y minutos
 
---
+++ solutions.ts
@@ -0,0 +1,11 @@
+function numbersSequence(step: number, limit: number): number[] {
+    const arrayFinal = []
+      
+    for (let i = step; i <= limit; i+=step) {
+        arrayFinal.push(i) 
+    }
+    return arrayFinal
+  }
+  
+  console.log(numbersSequence(3, 20))
+  
